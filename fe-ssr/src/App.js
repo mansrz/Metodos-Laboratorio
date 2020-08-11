ab5624c002b2b11af3e0f55590f782e35315929f
mansrz <mansrz@gmail.com>
2020-08-11 17:37:06 -0500

    upload first project

---
+++ fe-ssr/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <p>Prueba</p>
+    </div>
+  );
+}
+
+export default App;
